04169a4e174d0b95cc9c0fd7047913d6f60b0e3e
Mei Yifan <64300296+iatbsky@users.noreply.github.com>
2023-11-29 15:42:49 +0800

    Update update.py

--- update.py
+++ update.py
@@ -25,6 +25,7 @@ def gettoken(refresh_token):
          }
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
+    print(jsontxt)
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
     with open(path, 'w+') as f:
